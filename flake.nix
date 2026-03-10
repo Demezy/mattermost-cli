0b713ae8db499e39b614060a969dc61036fc2e8d
Demezy <38487319+Demezy@users.noreply.github.com>
2026-03-10 14:49:40 +0300

    refactor(flake): extract mkMm helper to deduplicate derivation

--- flake.nix
+++ flake.nix
@@ -20,47 +20,45 @@
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgsFor = system: nixpkgs.legacyPackages.${system};
     in
+    let
+      mkMm =
+        pkgs:
+        pkgs.stdenv.mkDerivation {
+          pname = "mm";
+          version = (builtins.fromJSON (builtins.readFile ./package.json)).version;
+          src = ./.;
+
+          nativeBuildInputs = [
+            pkgs.bun
+            pkgs.makeWrapper
+          ];
+
+          dontStrip = true;
+          dontFixup = true;
+
+          buildPhase = ''
+            runHook preBuild
+            export HOME=$TMPDIR
+            bun install --frozen-lockfile
+            bun build --compile bin/mm.ts --outfile mm
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            install -Dm755 mm $out/bin/mm
+            runHook postInstall
+          '';
+        };
+    in
     {
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = pkgsFor system;
-        in
-        {
-          mm = pkgs.stdenv.mkDerivation {
-            pname = "mm";
-            version = (builtins.fromJSON (builtins.readFile ./package.json)).version;
-            src = ./.;
-
-            nativeBuildInputs = [
-              pkgs.bun
-              pkgs.makeWrapper
-            ];
-
-            dontStrip = true;
-            dontFixup = true;
-
-            buildPhase = ''
-              runHook preBuild
-              export HOME=$TMPDIR
-              bun install --frozen-lockfile
-              bun build --compile bin/mm.ts --outfile mm
-              runHook postBuild
-            '';
-
-            installPhase = ''
-              runHook preInstall
-              install -Dm755 mm $out/bin/mm
-              runHook postInstall
-            '';
-          };
-
-          default = self.packages.${system}.mm;
-        }
-      );
+      packages = forAllSystems (system: {
+        mm = mkMm (pkgsFor system);
+        default = self.packages.${system}.mm;
+      });
 
       overlays.default = final: prev: {
-        mm = self.packages.${final.system}.mm;
+        mm = mkMm final;
       };
     };
 }
